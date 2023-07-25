2693332d242838de36c264224f8fa2e4cc277ba0
heroku121 <130366513+heroku121@users.noreply.github.com>
2023-07-25 15:55:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,201 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Lean Belly Juice</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="robots" content="noindex">
+    <link rel="icon" type="image/x-icon" href="./Lean-Belly-Juice_files/favicon.png">
+    <link rel="stylesheet" href="./Lean-Belly-Juice_files/style.css">
+    <link rel="stylesheet" href="./Lean-Belly-Juice_files/font-awesome.min.css">
+    <script type="text/javascript" src="./Lean-Belly-Juice_files/track.js.download"></script>
+
+</head>
+
+<body>
+    <div class="hed">
+        <div class="content">
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h2 style="color: #fff"> <u>ENJOY THE IKARIA LEAN BELLY JUICE FOR 75% OFF TODAY.</u></h2>
+            </a>
+        </div>
+    </div>
+
+    <div class="introd pd">
+        <div class="content">
+            <div class="introd-con flex justify flex-dir gap">
+                <div class="int-img"> <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/image06.jpg"></a></div>
+                <div class="info">
+                    <h2 class="main-pd">Lean Belly Juice</h2>
+                    <h2><b>A Scientifically <span style="color: #e43f3f">proven formula is 100% safe </span>and has Natural Proprietary.</b></h2>
+
+                    <ul class="lis-ch">
+                        <li>Supports Healthy Weight loss</li>
+                        <li>Boosts Your Energy Levels</li>
+                        <li>Made With 100% All-Natural Ingredients</li>
+                        <li>It is a 100% risk-free formula.</li>
+                    </ul>
+
+
+                    <div>
+                        <a href="https://bestofferhere.link/ikarialeanbelly">
+                            <h4 class="high"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+                        </a>
+                        <div class="ct-cen"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/cta-180.png" style="max-width: 300px"></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="what pd">
+        <div class="content">
+            <h1>What is Lean Belly Juice</h1>
+            <p>The delicious, all-natural blend of rare fruit and plant extracts that allows you to burn off stubborn fat without any extra effort... Allowing you to finally have the good health, toned and tight body you deserve. Simply mix one scoop of
+                powder into a glass of water or your favorite beverage each morning. The tasty juice will infuse your body with an ancient, youth-enhancing blend of rare nutrients that will activate your metabolism and give you a surge of powerful natural
+                energy.</p>
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h4 class="high"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+            </a>
+        </div>
+    </div>
+
+    <div class="cont ">
+        <div class="content flex align flex-dir gap">
+
+            <div class="why-cn">
+                <h1>How Ikaria Lean Belly Juice Works</h1>
+                <p>Ikaria Lean Belly Juice contains rare fruit and plant extract that is scientifically proven to help your body enter fat-burning mode by enhancing fat oxidation.</p>
+
+                <p>When your body converts food into energy rather than resulting in fat accumulation, you start shedding belly fat.</p>
+
+                <p>Simply mix one scoop of powder into a glass of water or your favorite beverage each morning.</p>
+
+                <div class="ct-cen"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/cta-180.png" style="max-width: 300px"></a></div>
+
+            </div>
+            <div class="why-img"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/work.png"></a></div>
+        </div>
+    </div>
+    <!-----benifits--->
+    <div class="bens pd">
+        <div class="content">
+            <h1>Benefits Of Prodentim</h1>
+            <ul class="lis-ch pl-0">
+                <li>It is a safe product with natural ingredients. It doesn’t have any side effects.</li>
+                <li>It Helps To Improve Joint Health</li>
+                <li>It Helps To Boost Your Energy Levels</li>
+                <li>It Control High Uric Acid Levels.</li>
+                <li>It Helps Prevent Fatty Liver Disease</li>
+                <li>It Boosts Your Metabolism</li>
+                <li>It Supports Ideal Blood Pressure</li>
+            </ul>
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h4 class="high"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+            </a>
+        </div>
+    </div>
+    <!-----benifits--->
+
+
+
+    <div class="ing pd">
+        <div class="content ">
+            <div class="txt-cen">
+                <h1>Ikaria Lean Belly Juice Ingredients</h1>
+                <p style="max-width: 800px;margin: 0 auto 20px">Ikaria Lean Belly Juice contains four proprietary blends, including metabolic blend, probiotic blend, digestive blend, and polyphenol blend. These blends are made with natural ingredients offering a complete metabolic makeover. Here are
+                    some of the ingredients from this formula.</p>
+            </div>
+            <div class=" ing-infsss flex gap flex-dir">
+                <div class="ing-boxes txt-cen">
+                    <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/ing1.png"></a>
+                    <h2>Milk Thistle</h2>
+                    <p>Milk thistle contains an active ingredient called silymarin that actively supports liver health. The liver is the most powerful fat-burning organ in the body because it metabolizes fat and breaks down stubborn visceral fat pockets.</p>
+                </div>
+                <div class="ing-boxes txt-cen">
+                    <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/ing2.png"></a>
+                    <h2>Taraxacum</h2>
+                    <p>This fascinating nutrient flushes out clogged fat by promoting optimal digestion and driving out dangerous ceramides from deep within your body.</p>
+                </div>
+                <div class="ing-boxes txt-cen">
+                    <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/ing3.png"></a>
+                    <h2>Panax Ginseng</h2>
+                    <p>This herb induces the production of gut bacteria that changes the pattern of calorie burn, which shrinks fat cells, boosts metabolism and accelerates weight loss.</p>
+                </div>
+            </div>
+            <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/cta-180.png" style="max-width: 300px;display: flex;margin: auto"></a>
+
+
+        </div>
+    </div>
+    <div class="gurs pd">
+        <div class="content">
+            <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/gurs.png"></a>
+            <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/cta-180.png" style="max-width: 300px;display: flex;margin: auto"></a>
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h4 class="high txt-cen"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+            </a>
+        </div>
+    </div>
+    <div class="faqs ">
+        <div class="content">
+
+            <h1>Frequently Asked Questions?</h1>
+            <h3>Is The Ikaria Lean Belly Juice For Real?</h3>
+            <p>The Ikaria Lean Belly Juice is an all-natural formula that contains only ingredients that are tested and produced in a controlled facility. It is manufactured here in the USA in an FDA-registered facility that follows the GMP (good manufacturing
+                practices) guidelines. The ingredients they use are of the highest possible standard and tested for the highest purity and potency. As with any dietary supplement, it is recommended that you consult with your physician before taking Ikaria
+                Lean Belly Juice.</p>
+
+            <h3>Can I Take The Ikaria Lean Belly Juice With Other Supplements?</h3>
+            <p>The Ikaria Lean Belly Juice should be compatible with other supplements without any problems or adverse effects. However, they do not recommend you take Lean Belly Juice together with supplements containing similar ingredients.</p>
+
+            <h3>Will The Ikaria Lean Belly Juice Really Work For Me?</h3>
+            <p>The Ikaria Lean Belly Juice is proven to support healthy weight loss, maintain healthy digestion and energy levels. It’s a powerful and unique blend that is helping thousands of men and women in many different countries. It makes no difference
+                if you’re a man or a woman, if you’re 60 or 30 years of age. You can get impressive results by taking the Ikaria Lean Belly Juice daily, ideally for 3 to 6 months. You could notice the difference in a few short days or in a week. Maybe
+                it’ll take a month or two to notice the impressive results you dream of by taking the Ikaria Lean Belly Juice each day. Either way, they believe you’ll experience great results from the formula, thanks to its unique combination of powerful
+                nutrients.</p>
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h4 class="high"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+            </a>
+        </div>
+    </div>
+
+    <div class="cst pd">
+        <div class="content">
+            <div class="txt-cen">
+                <h1>Special Time Offer!</h1>
+                <p style="max-width: 800px;margin: 0 auto 20px">Secure Your Reserved Ikaria Lean Belly Juice While Stock Last</p>
+            </div>
+            <div class="cst-inf flex gap flex-dir" style="margin-bottom: 30px">
+                <div class="cst-box"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/1.jpg"></a></div>
+                <div class="cst-box"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/3.jpg"></a></div>
+                <div class="cst-box"><a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/6.jpg"></a></div>
+            </div>
+            <a href="https://bestofferhere.link/ikarialeanbelly"><img src="./Lean-Belly-Juice_files/cta-180.png" style="max-width: 300px;display: flex;margin: auto"></a>
+            <a href="https://bestofferhere.link/ikarialeanbelly">
+                <h4 class="high txt-cen"><u>Enjoy Ikaria Lean Belly Juice™ 75% Off Today</u></h4>
+            </a>
+        </div>
+    </div>
+
+
+    <div class="footer txt-cen pd">
+        <div class="content">
+            <div class="foot-link ">
+                <a href="">Privacy Policy</a> | <a href="">Terms</a>| <a href="">Contact</a>
+            </div>
+
+            <small>Copyright @ 2023 Ikaria Juice – All rights reserved.</small>
+        </div>
+    </div>
+
+</body>
+
+</html>
+';
+?>
